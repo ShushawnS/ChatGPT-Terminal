eebef0735e55c04c41efdc588fceb386be3bb3ba
Shushawn Saha <shushawn_saha@trendmicro.com>
2022-12-08 21:39:21 -0500

    working conv func

--- chat/cmd/conv/conv.go
+++ chat/cmd/conv/conv.go
@@ -1,21 +1,92 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package conv
 
 import (
-	//"fmt"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
 
+	"github.com/sirupsen/logrus"
+	chatgpt_go "github.com/zhan3333/chatgpt-go"
+	"github.com/inancgumus/screen"
 	"github.com/spf13/cobra"
 )
+
+type loginData struct {
+	Email    string `json:email`
+	Password string `json:password`
+	ApiKey   string `json:apikey`
+}
+
+func getLoginData() (lData *loginData) {
+	filePath := "../config.json"
+	jsonFile, err := os.Open(filePath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var loginData loginData
+
+	json.Unmarshal(byteValue, &loginData)
+
+	return &loginData
+
+}
 
 // ConvCmd represents the conv command
 var ConvCmd = &cobra.Command{
 	Use:   "conv",
 	Short: "Converse with OpenAI's ChatGPT",
-	Long: ``,
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
+		screen.Clear()
+		screen.MoveTopLeft()
+
+		fmt.Println("\nConversation with ChatGPT ( type 'bye' to exit )")
+		fmt.Println("=================================================")
+
+		loginData := getLoginData()
+		//fmt.Println("Current Login Data: (email) %s (password) %s (apikey) %s", loginData.Email, loginData.Password, loginData.ApiKey)
+		bye := false
+		reader := bufio.NewScanner(os.Stdin)
+		timeout := time.Second * 60
+		client, err := chatgpt_go.NewChatGPT(loginData.ApiKey, chatgpt_go.ChatGPTOptions{
+			Log:     logrus.NewEntry(logrus.StandardLogger()),
+			Timeout: &timeout,
+		})
+		if err != nil {
+			panic(err)
+		}
+		conversation := client.NewConversation("", "")
+
+		for !bye {
+			fmt.Print("\nMe: ")
+
+			reader.Scan()
+
+			userText := reader.Text()
+
+			if userText == "bye" {
+				bye = true
+			} else {
+				resp, err := conversation.SendMessage(userText)
+				if err != nil {
+					panic(err)
+				}
+				fmt.Println("ChatGPT: \n", resp)
+			}
+
+		}
+
 		cmd.Help()
 	},
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,17 @@ module github.com/shushawns/ChatGPT-Terminal
 go 1.19
 
 require (
+	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3
+	github.com/sirupsen/logrus v1.9.0
+	github.com/spf13/cobra v1.6.1
+	github.com/zhan3333/chatgpt-go v0.0.0-20221208155043-a092f61d2422
+)
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
-	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/term v0.3.0 // indirect
 )
